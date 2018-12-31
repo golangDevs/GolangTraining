e74859e90eafeddcc83dca134b5df76ee3b1f308
golangDevs <46283867+golangDevs@users.noreply.github.com>
2019-01-01 00:08:06 +0530

    Creating package and maintain

---
+++ Go Package/SimplePackage/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com\srikant-prasad\go_crase_course\01_hello"
+)
+
+func main() {
+	Simple.Demo()
+	fmt.Println("I am now creating simple go package.")
+}
---
+++ Go Package/SimplePackage/simple/simpleTwo.go
@@ -0,0 +1,7 @@
+package simple
+
+import "fmt"
+
+func simpleTwo() {
+	fmt.Println("This package Simple programming.")
+}
